a5b85d6bc754f62e15e11389d21482f387a4f3eb
Ran Porath <ranporath@gmail.com>
2021-03-17 14:46:42 +0200

    nav bar

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -9,7 +9,7 @@
 <script>
 
 import appHeader from './cmps/app-header'
-import appFooter from './cmps/app-header'
+import appFooter from './cmps/app-footer'
 
 export default {
   components: {
---
+++ frontend/src/cmps/app-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+      <div className="footer">
+        <h3>© 2021 Stay. All rights reserved · Privacy · Terms · Sitemap</h3>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+//   name: 'footer',
+};
+</script>
