8ad69c00e6dac83507b79246265fd73b6bb9d2d6
Bari Avni <avnibari@gmail.com>
2021-03-17 14:53:50 +0200

    general changes

---
+++ frontend/src/store/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import { userStore } from './user.store.js'
+import { orderStore } from './order.store.js'
+import { stayStore } from './stay.store.js'
+import { tripStore } from './trip.store.js'
+// import { socketStore } from './socket.store.js'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  strict: true,
+  modules: {
+    userStore,
+    orderStore,
+    stayStore,
+    tripStore
+    // socketStore
+  },
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  }
+})
