4fffd0437acd1e2815f4e3883d178c9360f6d3e8
Ran Porath <ranporath@gmail.com>
2021-03-18 17:25:46 +0200

    bla

--- frontend/src/cmps/stay-preview.vue
+++ frontend/src/cmps/stay-preview.vue
@@ -1,9 +1,10 @@
     <template>
   <section class="stay-preview flex column clickable" @click="openDetails">
+    <img :src="loadImg"/>
+    <p :class="{opacity:noReviews}">{{ rating }}</p>
     <p>{{ stay.name }}</p>
     <p>{{ stay.capacity }} guests</p>
-    <p>${{ stay.price }} / night</p>
-    <img :src="loadImg"/>
+    <p><span class="bold">${{ stay.price }}</span> / night</p>
     <!-- <img class="card" src="@/assets/img/stay1.jpg"/> -->
   </section>    
 </template>
@@ -13,12 +14,18 @@ export default {
   computed: {
     loadImg() {
       return this.stay.imgUrls[0]
+    },
+    rating() {
+      return this.stay.reviews.length? '⭐ ' + this.stay.reviews[0].rate + ' (' + this.stay.reviews.length + ')' : 'no reviews yet'
+    },
+    noReviews() {
+      return !this.stay.reviews.length 
     }
   },
   methods: {
     openDetails() {
       this.$router.push(`/stay/${this.stay._id}`)
-    }
+    },
   }
 }
 </script>
