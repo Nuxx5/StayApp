5074f3dd42962c2ef836148286921a10f2e5a552
Bari Avni <avnibari@gmail.com>
2021-03-18 13:29:00 +0200

    home page

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,6 +1,38 @@
 <template>
   <section class="container home text-center">
-    <img src="" alt="" />
+    <img src="@/assets/img/hero.jpg" alt="" />
+    <div class="home-container">
+      <h2>Explore Locations</h2>
+      <div class="home-card">
+        <ul class="clean-list flex card-grid">
+          <li class="card">
+            <img src="@/assets/img/canada.jpg" alt="" />
+            <h3>Toronto</h3>
+          </li>
+          <li class="card">
+            <img src="@/assets/img/canada.jpg" alt="" />
+            <h3>Toronto</h3>
+          </li>
+          <li class="card">
+            <img src="@/assets/img/canada.jpg" alt="" />
+            <h3>Toronto</h3>
+          </li>
+        </ul>
+      </div>
+      <div class="home-about">
+        <h2>About Stay.</h2>
+        <div class="about-txt flex">
+          <div class="about-txt-card">
+            <h3>Who are we</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perferendis dignissimos nihil, id, sequi repudiandae doloribus nisi culpa nulla tempora a harum aliquam ab, iusto qui minus eaque nostrum? Voluptates.</p>
+          </div>
+          <div class="about-txt-card">
+            <h3>Our service</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perferendis dignissimos nihil, id, sequi repudiandae doloribus nisi culpa nulla tempora a harum aliquam ab, iusto qui minus eaque nostrum? Voluptates.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <h1>Stay.</h1>
     <h5 class="text-center">
       Anywhere. 
