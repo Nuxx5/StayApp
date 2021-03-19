6d5da96610f49ff69978f209872445b4928c2c36
Ran Porath <ranporath@gmail.com>
2021-03-20 01:09:27 +0200

    last friday

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,12 @@ import app from './app.vue'
 import {router} from './router'
 import {store} from './store/store'
 import './assets/styles/main.scss'
+import VCalendar from 'v-calendar';
+
+// Use v-calendar & v-date-picker components
+Vue.use(VCalendar, {
+  componentPrefix: 'vc'  // Use <vc-calendar /> instead of <v-calendar />
+});
 
 Vue.config.productionTip = false
 
--- frontend/src/views/stay-details.vue
+++ frontend/src/views/stay-details.vue
@@ -2,38 +2,73 @@
   <section v-if="stay" class="main-container stay-details flex column">
     <h2 class="room-title">{{ stay.name }}</h2>
     <div class="ratings flex">
-      <div class="details-rating" :class="{opacity:noReviews}">{{ rating }}</div>
-      <div>{{ stay.loc.address }}</div>
+      <div class="details-rating" :class="{ opacity: noReviews }">
+        {{ rating }}
       </div>
-      <div class="image-gallery">
-        <img class="image-gallery img1" :src="stay.imgUrls[2]">
-        <img class="image-gallery img2" :src="stay.imgUrls[2]">
-        <img class="image-gallery img3" :src="stay.imgUrls[2]">
-        <img class="image-gallery img4" :src="stay.imgUrls[2]">
-        <img class="image-gallery img5" :src="stay.imgUrls[2]">
+      <div>{{ stay.loc.address }}</div>
+    </div>
+    <div class="image-gallery">
+      <img class="image-gallery img1" :src="stay.imgUrls[2]" />
+      <img class="image-gallery img2" :src="stay.imgUrls[2]" />
+      <img class="image-gallery img3" :src="stay.imgUrls[2]" />
+      <img class="image-gallery img4" :src="stay.imgUrls[2]" />
+      <img class="image-gallery img5" :src="stay.imgUrls[2]" />
+    </div>
+    <div class="main-content flex space-between">
+      <div class="room-details flex column">
+        <div class="room-summary">
+          <p class="summary-text1">
+            Entire place hosted by {{ stay.host.fullname }}
+          </p>
+          <p class="summary-text2">{{ stay.capacity }} guests</p>
+          <img class="summary-img" :src="stay.host.imgUrl" />
         </div>
-        <div class="main-content flex space-between">
-          <div class="room-details flex column">
-            <div class="room-summary">
-              <p class="summary-text1">Entire place hosted by {{stay.host.fullname}}</p>
-              <p class="summary-text2">{{stay.capacity}} guests</p>
-              <img class="summary-img" :src="stay.host.imgUrl">
-              </div>
-              <div class="room-desc flex column">
-                <p>{{stay.summary}}</p>
-                </div>
-           </div>
-        <tripSettings @reservationMade="handleReservation" />
+        <div class="room-desc flex column">
+          <p>{{ stay.summary }}</p>
+        </div>
+        <div class="amenity-section">
+          <h1>Amenities</h1>
+          <div class="amenity-grid">
+            <div class="amenity-item">
+              <img src="../assets/img/Parking.f081ec43.svg" />
+              <span>Parking</span>
+            </div>
+            <div class="amenity-item">
+              <img src="../assets/img/Air conditioning.b1c18665.svg" />
+              <span>Air conditioning</span>
+            </div>
+            <div class="amenity-item">
+              <img src="../assets/img/TV.e782c1fe.svg" />
+              <span>TV</span>
+            </div>
+            <div class="amenity-item">
+              <img src="../assets/img/Wifi.6d265c21.svg" />
+              <span>Wifi</span>
+            </div>
+            <div class="amenity-item">
+              <img src="../assets/img/Kitchen.9817fe67.svg" />
+              <span>Kitchen</span>
+            </div>
+            <div class="amenity-item">
+              <img src="../assets/img/Essentials.223b18be.svg" />
+              <span>Essentials</span>
+            </div>
           </div>
-    <h2><span>amenities: </span>{{ stay.amenities }}</h2>
+        </div>
+      <datepicker :value="Date.now()"></datepicker>
+      </div>
+      <tripSettings @reservationMade="handleReservation" />
+    </div>
+
     <!-- <chat-app :stay="stay" /> -->
-    <router-link to="/stay" class="btn">Back</router-link>
+    <!-- <router-link to="/stay" class="btn">Back</router-link> -->
   </section>
 </template>
     <script>
 import { stayService } from "../services/stay.service.js";
+import Datepicker from "vuejs-datepicker";
 // import chatApp from "../cmps/chat-app.vue";
-import tripSettings from "../cmps/trip-settings.vue"
+import tripSettings from "../cmps/trip-settings.vue";
 export default {
   data() {
     return {
@@ -42,9 +77,8 @@ export default {
   },
   created() {
     const stayId = this.$route.params.id;
-    stayService.getStayById(stayId)
-    .then((stay) => {
-      console.log(stay)
+    stayService.getStayById(stayId).then((stay) => {
+      console.log(stay);
       this.stay = stay;
     });
   },
@@ -54,20 +88,27 @@ export default {
         return new Date(this.stay.createdAt).toLocaleDateString("he-IS");
     },
     rating() {
-      return this.stay.reviews.length? '⭐ ' + this.stay.reviews[0].rate + ' (' + this.stay.reviews.length + ')' : 'no reviews yet'
+      return this.stay.reviews.length
+        ? "⭐ " +
+            this.stay.reviews[0].rate +
+            " (" +
+            this.stay.reviews.length +
+            ")"
+        : "no reviews yet";
     },
     noReviews() {
-      return !this.stay.reviews.length 
-    }
+      return !this.stay.reviews.length;
+    },
   },
   components: {
     // chatApp,
-    tripSettings
+    tripSettings,
+    Datepicker,
   },
-  methods:{
-    handleReservation(reservation){
-      console.log(reservation)
-    }
-  }
+  methods: {
+    handleReservation(reservation) {
+      console.log(reservation);
+    },
+  },
 };
 </script>
